21c73d4fb218cefa56b3eb7baaef7f0bad129709
debora <debora@DESKTOP-5OC1DOK>
2017-10-31 20:14:40 +0700

    new commit

---
+++ algodattugasdebora/src/algodattugasdebora/Algodattugasdebora.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algodattugasdebora;
+
+/**
+ *
+ * @author Fiki L
+ */
+public class Algodattugasdebora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
